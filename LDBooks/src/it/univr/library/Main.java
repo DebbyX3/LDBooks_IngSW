70492fcfa0fe9cd61fc5e0fdf5f226d82db5d78b
DebbyX3 <p.debby100@yahoo.it>
2020-02-25 18:26:38 +0100

    Added Main class diagram

--- LDBooks/src/it/univr/library/Main.java
+++ LDBooks/src/it/univr/library/Main.java
@@ -5,68 +5,17 @@ import it.univr.library.Utils.StageManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-/*
-    Per caricare su github: (si usa prima di tutto git status)
-    1) git add nome_file
-    2) git commit -m "messaggio che indica la modifica effettuata"
-    3) git push per uploadare il file
-
-    Per scaricare (assicurarsi essere sincronizzati con github)
-*/
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-
+    public void start(Stage primaryStage)
+    {
         StageManager homePageStage = new StageManager();
         homePageStage.setStageHomepage(primaryStage);
-
-
-
-        /* SCHERMATA INIZIALE PRECEDENTE
-        StageManager catalogStage = new StageManager();
-        catalogStage.setStageCatalog(primaryStage, null);
-        */
-        /*ControllerCatalog catalog = new ControllerCatalog();
-        catalog.setSceneCatalog(primaryStage);*/
-
-        /*Parent root = FXMLLoader.load(getClass().getResource("fxml/catalog.fxml"));
-
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-
-        primaryStage.show();*/
-
-        /*
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/catalog.fxml"));
-        Parent root = loader.load();
-        ControllerCatalog controller = loader.getController();
-        controller.setStage(primaryStage);
-        */
-
-        /* metodo1
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/catalog.fxml"));
-
-        ControllerCatalog controller = new ControllerCatalog();   //da chiamare prima di set controller
-        loader.setController(controller);
-
-        Parent root = loader.load();
-        controller.setRoot(root);
-
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();*/
-
-
-        /* metodo 2
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/catalog.fxml"));
-        loader.setController(new ControllerCatalog());
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(loader.load()));
-        primaryStage.show();*/
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
 
         // Since we use the singleton pattern, we close the database connection here
